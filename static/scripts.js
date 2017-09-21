2fdc2eb0d5a0ba1c1021f2e6863343470dccd4a1
joostf <info@joostfaber.nl>
2017-09-21 16:14:56 +0200

    added iife for scoped variables

--- static/scripts.js
+++ static/scripts.js
@@ -1,17 +1,20 @@
+(function(){
+  var el = document.querySelector('aside > div')
+  var i = .01;
+  var pos = 120;
+
+  function preScroll() {
+    i += .035
+    el.scrollLeft += (10 * i);
+    if (el.scrollLeft<pos) {
+      requestAnimationFrame(preScroll)
+    }
+  }
+
+  requestAnimationFrame(preScroll)
+
+}());
+
 //quotes even laten scrollen
 //voor de 'discoverability' weetjewel - http://whatis.techtarget.com/definition/discoverability-in-UX-design
 //beetje easing https://www.kirupa.com/html5/introduction_to_easing_in_javascript.htm
-
-var q = document.querySelector('aside > div')
-var i = .01;
-var pos = 120;
-
-function preScroll() {
-  i += .035
-  q.scrollLeft += (10 * i);
-  if (q.scrollLeft<pos) {
-    window.requestAnimationFrame(preScroll)
-  }
-}
-
-window.requestAnimationFrame(preScroll)
