b68a06e53368d621c67897675ce7eb3204aeff2c
joostf <info@joostfaber.nl>
2018-09-18 14:34:47 +0200

    changes

--- static/scripts.js
+++ static/scripts.js
@@ -1,30 +1,24 @@
-
 //quotes even laten scrollen
 //voor de 'discoverability' weetjewel - http://whatis.techtarget.com/definition/discoverability-in-UX-design
 //beetje easing https://www.kirupa.com/html5/introduction_to_easing_in_javascript.htm
 
-
-
-/*
-(function(){
+(function() {
   var comments = document.querySelector('aside > div'),
-      title = document.querySelector('h1'),
-      i = .01,
-      pos = 120
+    title = document.querySelector('h1'),
+    i = 0.01,
+    pos = 120;
 
   function preScroll() {
-    i += .035
-    comments.scrollLeft += (10 * i);
-    if (comments.scrollLeft<pos) {
-      requestAnimationFrame(preScroll)
+    i += 0.035;
+    comments.scrollLeft += 10 * i;
+    if (comments.scrollLeft < pos) {
+      requestAnimationFrame(preScroll);
     }
   }
 
-  requestAnimationFrame(preScroll)
+  requestAnimationFrame(preScroll);
 
   title.addEventListener('click', () => {
-    document.body.classList.toggle('show-comments')
-  })
-
-}())
-*/
+    document.body.classList.toggle('show-comments');
+  });
+})();
